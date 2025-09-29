735a3a3c0bb6f5b444fdc50e0120b7ea9aabd31c
Saikumar <karu3570@gmail.com>
2025-09-29 07:18:19 +0530

    Initial commit

---
+++ unit2/JS/function.js
@@ -0,0 +1,49 @@
+/*const greet = (name) => {
+    return `Hello ${name}..`;
+}
+
+console.log(greet("sai"));*/
+
+const add = (num1,num2) => {
+    return num1 + num2;
+}
+const sub = (num1,num2) => {
+    return num1 - num2;
+}
+const mul = (num1,num2) => {
+    return num1 * num2;
+}
+const div = (num1,num2) => {
+    return num1 / num2;
+}
+
+console.log(12);
+console.log(0b11);
+
+let str = 'sai';
+console.log(str);
+
+let student = true;
+console.log(!student);
+
+let val=null;
+console.log(val);
+
+let undefinedval;
+console.log(undefinedval);
+
+let bigNumber = 122798134934610347913234123545758358625472457465n;
+console.log(bigNumber);
+
+let person={
+    name:'sai',
+    age:20
+}
+console.log(person);
+
+let marks = 75;
+
+if(marks >= 50) console.log("pass");
+else console.log("fail");
+
+for(let i=1;i<=5;i++) console.log(i);
